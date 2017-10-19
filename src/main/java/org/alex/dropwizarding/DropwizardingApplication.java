5e4f482d28235f8af0f4f21611947736e465d481
Alex Pask <alex.pask@ladbrokescoral.com>
2017-10-19 22:56:35 +0100

    Initial Commit

---
+++ src/main/java/org/alex/dropwizarding/DropwizardingApplication.java
@@ -0,0 +1,23 @@
+package org.alex.dropwizarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
+
+@SpringBootApplication
+public class DropwizardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DropwizardingApplication.class, args);
+		List<String>
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+
+
+
+}
---
+++ src/test/java/org/alex/dropwizarding/DropwizardingApplicationTests.java
@@ -0,0 +1,16 @@
+package org.alex.dropwizarding;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DropwizardingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
